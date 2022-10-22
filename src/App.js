8dbb283c53c65880f2f1ca9355160e909c62fd59
reenphygeorge <reenphygeorge@gmail.com>
2022-10-22 21:15:02 +0530

    chore: First Build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heading } from '@chakra-ui/react';
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+import { VStack, IconButton } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { useState } from 'react';
+import { nanoid } from 'nanoid'
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo => {
+      return todo.id !== id
+    }))
+    setTodos(newTodos);
+  }
+
+  const insertTodo = (task) => {
+    var TaskObj = {
+      id: nanoid(),
+      body: task
+    }
+    var newTodos = []
+    newTodos.push(...todos)
+    newTodos.push(TaskObj)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={4}>
+      <IconButton mb='7' icon={<FaSun />} isRound='true' size="lg" alignSelf="flex-end" />
+      <Heading mb='8' fontWeight='bold' bgGradient='linear(to-r, pink.500, pink.300, blue.500)' bgClip='text'>Todo Application</Heading>;
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      <AddTodo insertTodo={insertTodo} />
+    </VStack>
   );
 }
 
---
+++ src/components/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { HStack, Input, Button, useToast } from '@chakra-ui/react'
+
+function AddTodo({ insertTodo }) {
+
+    const [Content, setContent] = useState('')
+    const toast = useToast();
+    var handleSubmit = () => {
+        if (!Content) {
+            toast({
+                title: 'No content',
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            })
+            return;
+        }
+        insertTodo(Content)
+    }
+
+    return (
+        <form>
+            <HStack mt='10'>
+                <Input variant='filled' placeholder='add it now' value={Content} onChange={(e) => setContent(e.target.value)} />
+                <Button px='8' colorScheme='pink' type='button' onClick={() => handleSubmit()}>Add Todo</Button>
+            </HStack>
+        </form>
+    )
+}
+
+export default AddTodo
