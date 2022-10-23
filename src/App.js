e8e0d1029b0c31d124d15e210dcef8147cb07df4
reenphygeorge <reenphygeorge@gmail.com>
2022-10-23 12:50:54 +0530

    feat: localstorage and dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, useColorMode } from '@chakra-ui/react';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import { VStack, IconButton } from '@chakra-ui/react';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid'
 
 function App() {
 
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem('todos')) || [])
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const { colorMode, toggleColorMode } = useColorMode()
 
   const deleteTodo = (id) => {
     const newTodos = todos.filter((todo => {
@@ -30,7 +35,7 @@ function App() {
 
   return (
     <VStack p={4}>
-      <IconButton mb='7' icon={<FaSun />} isRound='true' size="lg" alignSelf="flex-end" />
+      <IconButton mb='7' icon={colorMode === 'light' ? <FaSun /> : <FaMoon />} isRound='true' size="lg" alignSelf="flex-end" onClick={toggleColorMode} />
       <Heading mb='8' fontWeight='bold' bgGradient='linear(to-r, pink.500, pink.300, blue.500)' bgClip='text'>Todo Application</Heading>;
       <TodoList todos={todos} deleteTodo={deleteTodo} />
       <AddTodo insertTodo={insertTodo} />
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -16,6 +16,7 @@ function AddTodo({ insertTodo }) {
             return;
         }
         insertTodo(Content)
+        setContent('');
     }
 
     return (
